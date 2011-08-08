589532139be1aa36c44d8574770dbbf5634f57b9
Doug Moore <lordscarlet@idledreams.net>
2011-08-08 23:27:36 +0000

    Switched cursor to dom object instead of being drawn on the canvas
    git-svn-id: http://svn.smallvillage.net/js-textmode-editor/trunk@14 9b626a8c-83ae-47eb-b694-7da4a01b9bc0

--- src/js/editor.js
+++ src/js/editor.js
@@ -11,11 +11,17 @@
     this.Cursor = (function(){
 
       function Cursor() { };
+      Cursor.prototype.move = function() {
+        dom.animate({left: x*width}, 10);
+        dom.animate({top: y*height}, 10);
+        return true;
+      };
       Object.defineProperties(this, {
         "x": { writable: true },
         "y": { writable: true },
         "width": { writable: true },
-        "height": { writable: true }
+        "height": { writable: true },
+        "dom" : {writable: true}
       });
       return Cursor;
     });
@@ -28,42 +34,50 @@
       this.canvas.setAttribute('height', this.height);
 
       this.cursor = new Cursor();
+      this.cursor.dom = $("#cursor");
       this.cursor.width = 8;
       this.cursor.height = 16;
-      this.cursor.x = 0;
-      this.cursor.y = 0;
+      this.cursor.x = 1;
+      this.cursor.y = 1;
+      this.cursor.dom.width(this.cursor.width);
+      this.cursor.dom.height(this.cursor.height);
 
       if (this.canvas.getContext) {
         this.ctx = this.canvas.getContext('2d');
         //ctx.scale(.5,.5);
       }
-      setInterval("editor.draw()", 300);
+//      setInterval("editor.draw()", 9);
 
     $('body').bind('keydown', function(e) {
       key = {left:37, up:38, right:39, down:40, f1:112, f2:113, f3:114, f4:115, f5:116, f6:117, f7:118, f8:119, f9:120, f10:121, f11:122, f12:123};
       switch (e.which) {
         case key.left:
-          if (editor.cursor.x > editor.cursor.width) {
+          if (editor.cursor.x > editor.cursor.width/editor.cursor.width) {
             //eraseeditor.cursor(editor.cursor.x, editor.cursor.top);
-            editor.cursor.x = editor.cursor.x - editor.cursor.width;
+            editor.cursor.x--;
+            editor.cursor.dom.animate({left: editor.cursor.x*editor.cursor.width}, 10)
           }
           break;
         case key.right:
-          if (editor.cursor.x < editor.width) {
+          if (editor.cursor.x < editor.width/editor.cursor.width) {
             //eraseeditor.cursor(editor.cursor.left, editor.cursor.top);         
-            editor.cursor.x = editor.cursor.x + editor.cursor.width;
+            editor.cursor.x++;
+            editor.cursor.move();
+            //editor.cursor.dom.animate({left: (editor.cursor.x*editor.cursor.width)}, 10)
           }
           break;
         case key.down:
-          if (editor.cursor.y < editor.height - editor.cursor.height) { // For now act on a fixed size canvas
+          if (editor.cursor.y < (editor.height - editor.cursor.height)/editor.cursor.height) { // For now act on a fixed size canvas
             //eraseeditor.cursor(editor.cursor.left, editor.cursor.top);
-            editor.cursor.y = editor.cursor.y + editor.cursor.height;
+            editor.cursor.y++;
+            editor.cursor.dom.animate({top: editor.cursor.y*editor.cursor.height}, 10)
           }
           break;
         case key.up:
-          if (editor.cursor.y > editor.cursor.height) {
+          if (editor.cursor.y > 0) {
             //eraseeditor.cursor(editor.cursor.left, editor.cursor.top);
-            editor.cursor.y = editor.cursor.y - editor.cursor.height ;
+            editor.cursor.y--;
+            editor.cursor.dom.animate({top: editor.cursor.y*editor.cursor.height}, 10)
           }
           break;
         case key.f5:
@@ -74,8 +88,10 @@
 
     };
     Editor.prototype.draw = function() {
+      this.ctx.fillStyle = "#000000";
+      this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
       this.ctx.fillStyle = "#ababab";
-      this.ctx.fillRect(this.cursor.x,this.cursor.y,this.cursor.width,this.cursor.height);
+      this.ctx.fillRect(this.cursor.x*this.cursor.width,this.cursor.y*this.cursor.height,this.cursor.width,this.cursor.height);
       this.ctx.fill();
       return true;
     };
