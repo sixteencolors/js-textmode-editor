c0c7a058d22684c7e92b0b32aebdec95604e6d58
Doug Moore <lordscarlet@idledreams.net>
2011-08-09 00:03:25 +0000

    Cleaned up cursor movement and cursor object
    git-svn-id: http://svn.smallvillage.net/js-textmode-editor/trunk@17 9b626a8c-83ae-47eb-b694-7da4a01b9bc0

--- src/js/editor.js
+++ src/js/editor.js
@@ -10,21 +10,23 @@
   this.Editor = (function() {
     this.Cursor = (function(){
 
-      function Cursor() { };
+      function Cursor(w, h) {
+        this.width = w;
+        this.height = h;
+        this.x = 1;
+        this.y = 1;
+        this.dom = $("#cursor");
+        this.dom.width(this.width);
+        this.dom.height(this.height);
+        this.move();
+      };
       Cursor.prototype.move = function() {
-        dom.animate({left: x*width}, 10);
-        dom.animate({top: y*height}, 10);
+        this.dom.animate({left: this.x*this.width}, 10);
+        this.dom.animate({top: this.y*this.height}, 10);
         return true;
       };
-      Object.defineProperties(this, {
-        "x": { writable: true },
-        "y": { writable: true },
-        "width": { writable: true },
-        "height": { writable: true },
-        "dom" : {writable: true}
-      });
       return Cursor;
-    });
+    })();
     function Editor(w, h, id) {
       this.width = w;
       this.height = h;
@@ -33,14 +35,7 @@
       this.canvas.setAttribute('width', this.width);
       this.canvas.setAttribute('height', this.height);
 
-      this.cursor = new Cursor();
-      this.cursor.dom = $("#cursor");
-      this.cursor.width = 8;
-      this.cursor.height = 16;
-      this.cursor.x = 1;
-      this.cursor.y = 1;
-      this.cursor.dom.width(this.cursor.width);
-      this.cursor.dom.height(this.cursor.height);
+      this.cursor = new Cursor(8, 16);
 
       if (this.canvas.getContext) {
         this.ctx = this.canvas.getContext('2d');
@@ -48,43 +43,43 @@
       }
 //      setInterval("editor.draw()", 9);
 
-    $('body').bind('keydown', function(e) {
-      key = {left:37, up:38, right:39, down:40, f1:112, f2:113, f3:114, f4:115, f5:116, f6:117, f7:118, f8:119, f9:120, f10:121, f11:122, f12:123};
-      switch (e.which) {
-        case key.left:
-          if (editor.cursor.x > editor.cursor.width/editor.cursor.width) {
-            //eraseeditor.cursor(editor.cursor.x, editor.cursor.top);
-            editor.cursor.x--;
-            editor.cursor.dom.animate({left: editor.cursor.x*editor.cursor.width}, 10)
-          }
-          break;
-        case key.right:
-          if (editor.cursor.x < editor.width/editor.cursor.width) {
-            //eraseeditor.cursor(editor.cursor.left, editor.cursor.top);         
-            editor.cursor.x++;
-            editor.cursor.move();
-            //editor.cursor.dom.animate({left: (editor.cursor.x*editor.cursor.width)}, 10)
-          }
-          break;
-        case key.down:
-          if (editor.cursor.y < (editor.height - editor.cursor.height)/editor.cursor.height) { // For now act on a fixed size canvas
-            //eraseeditor.cursor(editor.cursor.left, editor.cursor.top);
-            editor.cursor.y++;
-            editor.cursor.dom.animate({top: editor.cursor.y*editor.cursor.height}, 10)
-          }
-          break;
-        case key.up:
-          if (editor.cursor.y > 0) {
-            //eraseeditor.cursor(editor.cursor.left, editor.cursor.top);
-            editor.cursor.y--;
-            editor.cursor.dom.animate({top: editor.cursor.y*editor.cursor.height}, 10)
-          }
-          break;
-        case key.f5:
-          alert("f5!");
-          break;
-      }
-    });
+      $('body').bind('keydown', function(e) {
+        key = {left:37, up:38, right:39, down:40, f1:112, f2:113, f3:114, f4:115, f5:116, f6:117, f7:118, f8:119, f9:120, f10:121, f11:122, f12:123};
+        switch (e.which) {
+          case key.left:
+            if (editor.cursor.x > editor.cursor.width/editor.cursor.width) {
+              //eraseeditor.cursor(editor.cursor.x, editor.cursor.top);
+              editor.cursor.x--;
+              editor.cursor.dom.animate({left: editor.cursor.x*editor.cursor.width}, 10)
+            }
+            break;
+          case key.right:
+            if (editor.cursor.x < editor.width/editor.cursor.width) {
+              //eraseeditor.cursor(editor.cursor.left, editor.cursor.top);         
+              editor.cursor.x++;
+              editor.cursor.move();
+              //editor.cursor.dom.animate({left: (editor.cursor.x*editor.cursor.width)}, 10)
+            }
+            break;
+          case key.down:
+            if (editor.cursor.y < (editor.height - editor.cursor.height)/editor.cursor.height) { // For now act on a fixed size canvas
+              //eraseeditor.cursor(editor.cursor.left, editor.cursor.top);
+              editor.cursor.y++;
+              editor.cursor.dom.animate({top: editor.cursor.y*editor.cursor.height}, 10)
+            }
+            break;
+          case key.up:
+            if (editor.cursor.y > 0) {
+              //eraseeditor.cursor(editor.cursor.left, editor.cursor.top);
+              editor.cursor.y--;
+              editor.cursor.dom.animate({top: editor.cursor.y*editor.cursor.height}, 10)
+            }
+            break;
+          case key.f5:
+            alert("f5!");
+            break;
+        }
+      });
 
     };
     Editor.prototype.draw = function() {
